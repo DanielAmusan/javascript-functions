770a6d2d8dc7265a0a5b5ff34d653e52962b34db
Daniel Amusan <86704130+DanielAmusan@users.noreply.github.com>
2021-11-08 13:51:24 +0100

    Update gameoflife.js

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,7 +1,7 @@
 function seed() {
 return Array.prototype.slice.call(arguments)}
 function same([x, y], [j, k]) {
-  return x === y && j === k;
+  return x === j && y === k;
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
